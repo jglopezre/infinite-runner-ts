bd9c86d12440c6a5d80ebe98f00d8116a76513a6
Javier Lopez <javierlopez_85@hotmail.com>
2023-02-15 00:39:08 -0300

    background scroll added

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,23 +1,41 @@
 import Phaser from "phaser";
 import AnimationsKeys from "../consts/AnimationKeys";
-
-
+import TextureKeys from "../consts/TextureKeys";
 
 export default class Game extends Phaser.Scene {
-  BACKGROUND = 'background'
   
+  #background!: Phaser.GameObjects.TileSprite //using # instead of PRIVATE, for javascript private props
+
   constructor() {
     super('game')
   }
 
   create() {
-    
-
     const width = this.scale.width
     const height = this.scale.height
 
-    this.add.tileSprite(0, 0, width, height, this.BACKGROUND).setOrigin(0)
+    this.#background = this.add.tileSprite(0, 0, width, height, TextureKeys.Background)
+      .setOrigin(0, 0)
+      .setScrollFactor(0, 0)
 
-    this.physics.add.sprite(width * 0.5, height * 0.5, 'player').play(AnimationsKeys.RocketMouseRun)
+    const mouse = this.physics.add.sprite(width * 0.5, height * 0.5, TextureKeys.RocketMouse)
+      .play(AnimationsKeys.RocketMouseRun)
+
+    const body = mouse.body as Phaser.Physics.Arcade.Body  //Type-Casting, important to styudy
+    body.setCollideWorldBounds(true)
+
+    body.setVelocityX(200)
+
+    this.physics.world.setBounds(
+      0, 0,
+      Number.MAX_SAFE_INTEGER, height -30
+    )
+
+    this.cameras.main.startFollow(mouse)
+    this.cameras.main.setBounds(0, 0, Number.MAX_SAFE_INTEGER, height)
+  }
+
+  update(t: number, dt:number) {
+    this.#background.setTilePosition(this.cameras.main.scrollX)
   }
 }
